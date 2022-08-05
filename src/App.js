7f9d42571e782cfb7a1c830c42e1ead55f713650
mahsajafari3855 <mahsa.jafari3855@gmail.com>
2022-08-05 23:04:01 +0430

    create upload and download with react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [selectedfile, setselectedFile] = useState(null);
+  const onChangeHandler = (event) => {
+    console.log("firstfile", event.target.files[0]);
+    const file = event.target.files[0];
+    setselectedFile(file);
+    console.log("selectedFile", selectedfile);
+  };
+  const onClickHandler = () => {
+    const data = new FormData();
+    data.append("file", selectedfile);
+    axios
+      .post("http://localhost:8000/upload", data, {
+        // receive two parameter endpoint url ,form data
+      })
+      .then((res) => {
+        // then print response status
+        console.log(res.statusText);
+      });
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Upload&Download with React js</p>
+        <input type="file" name="file" onChange={onChangeHandler} />
+        <button className="btn" onClick={onClickHandler}>
+          Upload
+        </button>
+
+        <button className="btn" href="/somefile.txt" download>
+          <a href="/somefile.txt" download>
+            Click to download
+          </a>
+        </button>
       </header>
     </div>
   );
